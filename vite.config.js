54ff6c43f5c367bcc24062359185804cafd9fe1b
nzaoo <nzao1327@gmail.com>
2025-06-23 16:45:05 +0700

    🎮 Initial commit for maze game

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/mini_game_maze_3d/', // Đúng tên repo GitHub Pages
+  base: '/mini_game_maze_3d/', // PHẢI đúng với tên repo GitHub
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: resolve(__dirname, 'index.html'),
     },
   },
-});
+})
